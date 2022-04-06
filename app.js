f44608b6116ecf135cf19a950d84541bcca74b48
Max <89795178+maxmandia@users.noreply.github.com>
2022-04-06 18:04:33 -0400

    more changes

--- app.js
+++ app.js
@@ -10,15 +10,27 @@ function playRound(playerChoice, computerChoice) {
     return "you tied";
   else if (playerChoice === "rock" && computerChoice === "scissors")
     return "you win";
+  else if (playerChoice === "paper" && computerChoice === "paper")
+    return "you tied";
+  else if (playerChoice === "paper" && computerChoice === "rock")
+    return "you win";
+  else if (playerChoice === "paper" && computerChoice === "scissors")
+    return "you lose";
+  else if (playerChoice === "scissors" && computerChoice === "paper")
+    return "you win";
+  else if (playerChoice === "scissors" && computerChoice === "rock")
+    return "you lose";
+  else if (playerChoice === "scissors" && computerChoice === "scissors")
+    return "you tied";
 }
 
 const buttons = document.querySelectorAll("button");
+
 buttons.forEach(function (btn) {
   btn.addEventListener("click", () => {
     playerChoice = btn.className;
     let computerChoice = computerPlay();
 
-    console.log(playerChoice);
-    console.log(computerChoice);
+    console.log(playRound(playerChoice, computerChoice));
   });
 });
